9c6f69afbe3f5fb8ca1a20b668ee7b1122b2d7b6
Your Name <you@example.com>
2022-07-25 23:00:52 -0500

    early code structures in the making

---
+++ Assets/javascript/script.js
@@ -0,0 +1,12 @@
+var questions = document.querySelector(".questions");
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+
+var questionUp = "";
+var winCounter = 0;
+var loseCounter = 0;
+var isWin = false;
+var timer;
+var timerCount;
